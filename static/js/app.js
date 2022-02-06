3060197dc9b5c78189c622a5eb56d5ed1f4a3f79
Eric Spoerner <espoerner@gmail.com>
2022-02-06 10:28:41 -0800

    Completed display of search criteria on primary website.  Initial dev complete.

--- static/js/app.js
+++ static/js/app.js
@@ -45,8 +45,8 @@ function updateFilters() {
       filters[elemID] = inputValue;
     } else {
       delete filters[elemID];
-    }
-    
+    };
+
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -56,15 +56,26 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
-  
+    // console.log(Object.keys(filters).length);
+
+    let filterKeys = Object.keys(filters);    
+    for (i = 0; i < filterKeys.length; i++) {
+      let filterKey = filterKeys[i];
+      let filterVal = filters[filterKey];
+      // Downside of approach: requires full word.
+      filteredData = filteredData.filter(row => row[filterKey] === filterVal);
+    };
+
+    console.log(filteredData);
+
     // 10. Finally, rebuild the table using the filtered data
-    
-  }
+    buildTable(filteredData);
+
+  };
   
   // 2. Attach an event to listen for changes to each filter
   // orig: d3.selectAll("#filter-btn").on("click", handleClick);
